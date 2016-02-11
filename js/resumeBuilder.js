d6e3a123f6af0c758d7b949b1d877d7808495ab7
GuillaumeS <guillaume.simler@gmail.com>
2016-02-11 11:14:19 +0100

    Clean up helper.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,128 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+
+var bio={"name": "Guillaume Simler", 
+		"role": "Web Student", 
+		"picture_URL": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/5/005/0ad/352/006b11c.jpg", 
+		"welcome_message" : "Welcome on my journey from corporate finance to IT",
+		"skills" :["Corporate Finance", "Team Management", "M&A", "Sales", "ERP"],
+		"contacts" :{
+			"mobile": "+49 176 76 24 ----", 
+			"email": "guillaume.simler@gmail.com", 
+			"LinkedIn": "https://de.linkedin.com/in/guillaume-simler-1391939",
+			"Xing": "https://www.xing.com/profile/Guillaume_Simler",
+			"Github":"https://github.com/guillaumesimler"}
+		};
+
+var projects={
+	"project":[
+		{"tile": "About Me",
+		"dates": "01/2016",
+		"description": "a first intro in HTML and CSS",
+		"images":[
+			"http://www.simler.eu/images/guillaume_project1-600_1x.jpg",
+			"http://www.simler.eu/images/guillaume_project1-400_1x.jpg"]
+		},
+
+		{"tile": "Portfolio",
+		"dates": "02/2016",
+		"description": "a deep dive into responsive webdesign",
+		"images":[
+			"http://www.simler.eu/images/guillaume_project2-600_1x.jpg",
+			"http://www.simler.eu/images/guillaume_project2-400_1x.jpg"]
+		}		
+	]
+};
+
+var work ={
+	"jobs":[
+		{"employer": "TTS Tooltechnic System (Festool)",
+		"title":"Head of Strategic Projects",
+		"location": ["Wendlingen, Germany"],
+		"dates" : "01/2014-03/2016",
+		"description": "responsible for corporate strategy process, M&A, unconventional projects (Fablab, ...)"
+		},
+
+		{"employer": "TTS Tooltechnic System (Festool)",
+		"title":"Managing Director TTS Polak",
+		"location": ["Mimon, Czech Republic"],
+		"dates" : "01/2012-12/2013",
+		"description": "responsible for daily operationss of TTS' aluminum foundry, its restruturing and sale to a strategic investor"
+		},
+
+		{"employer": "AVL",
+		"title":"Manager Controlling AVL Europe & CFO AVL France",
+		"location": [
+			"Wiesbaden, Germany", 
+			"Croissy-sur-Seine, France", 
+			"Barcelona, Spain", 
+			"Stockholm, Sweden", 
+			"Torino, Italy",
+			"Kidderminster, United Kingdom" 
+			"Graz, Austria"],
+		"dates" : "02/2008-12/2011",
+		"description": "responsible for daily financial operations of AVL France and its restruturing as well as a coordinating function of the financial operations of the European subsidies"
+		},
+
+		{"employer": "AGT Thermotechnik",
+		"title":"financial manager",
+		"location": ["Erkelenz, Germany"],
+		"dates" : "08/2007-06/2008",
+		"description": "responsible for daily financial operations of AGT Thermotechnik"
+		},
+
+		{"employer": "IBM",
+		"title":"ibm.com sales operation analyst",
+		"location": [
+			"Herrenberg, Germany", 
+			"Stuttgart, Germany", 
+			"Courbevoie, France"],
+		"dates" : "01/2005-09/2006",
+		"description": "responsible for reporting tool and method improvements, business counterpart to sales team"
+		}
+	]
+};
+
+var education={
+	"schools": [
+	{"name": "Audencia",
+	"graduation": 2007,
+	"city": "Nantes, France",
+	"degree": "Master in Science",
+	"majors": ["controlling", "marketing", "entrepreneurship"]
+
+	},
+	{"name": "WHU",
+	"graduation": 2007,
+	"city": "Vallendar, Germany",
+	"degree": "exchange year",
+	"majors": ["controlling", "marketing", "entrepreneurship"]
+	},
+
+	{"name": "Lycée du Parc",
+	"graduation": 2003,
+	"city": "Lyon, France",
+	"degree": "exchange year",
+	"majors": ["math", "History", "philosophy"],
+	"minors": ["German", "English", "Economics"]}
+	],
+
+	"OnlineCourses": [
+	{"title" : "Programming Foundations with Python",
+	"school" : "Udacity",
+	"dates": "12/2015",
+	"courseUrl": "https://www.udacity.com/courses/ud036"
+	},
+
+	{"title" : "How to Use Git and GitHub",
+	"school" : "Udacity",
+	"dates": "01/2016",
+	"courseUrl": "https://www.udacity.com/courses/ud775"
+	},
+
+	{"title" : "Introduction to Cloud Computing",
+	"school" : "edX",
+	"dates": "01/2016",
+	"courseUrl": "https://www.edx.org/course/introduction-cloud-computing-ieeex-cloudintro-x-0"
+	}
+	]
+};
+
